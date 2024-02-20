dec9fb3597f95fec26817dcfa6045827e517c951
Tyler Savitsky <tsavitsky11@gmail.com>
2024-02-20 17:13:40 -0500

    implemented some RTK boilerplate for our finance and news features

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hellooooooooooooo World!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App.jsx';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
