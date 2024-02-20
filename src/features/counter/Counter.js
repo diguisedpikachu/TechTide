a7648b8d986c5c9837b84a1e4ebe0c68132cfaf2
Brandon <brandon5.9@hotmail.com>
2024-02-20 13:43:39 -0500

    Setting app the store and the reducer to set up redux

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <div>
+                <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <spa
+            </div>
+        </div>
+
+
+    );
+}
